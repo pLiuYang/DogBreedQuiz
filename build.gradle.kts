69df199074d377fe30afd345a31ebea7514aa2c2
yang.liu76 <yang.liu76@bytedance.com>
2025-09-10 21:00:00 +0800

    Initial project setup with Gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
